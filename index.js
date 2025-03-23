6913a4fe832b2fe525228ce2af99fd6a2deeaf4a
alameesnr <145246885+alameesnr@users.noreply.github.com>
2025-03-23 22:01:51 +0100

    python week 2

--- index.js
+++ index.js
@@ -45,4 +45,64 @@ function greet(name) {
   console.log(user); // Output: { name: "Alamin", age: 26 }
   
   // user = { name: "New User" }; ❌ Error: Cannot reassign a const object
+
+
+
+//Embedding Objects & Arrays
+
+  const user = { name: "Alamin", role: "Backend Developer" };
+  console.log(`User: ${user.name}, Role: ${user.role}`);
+  // Output: User: Alamin, Role: Backend Developer
   
+  const numbers = [10, 20, 30];
+  console.log(`Array Values: ${numbers.join(", ")}`);
+  // Output: Array Values: 10, 20, 30
+  
+
+  //Using Template Literals in Functions
+  const greet = (name) => `Hello, ${name}! Hope you're enjoying JavaScript.`;
+  console.log(greet("Alamin"));
+  // Output: Hello, Alamin! Hope you're enjoying JavaScript.
+
+
+  //Expression Interpolation inside Template Literals
+
+const a = 5;
+const b = 10;
+console.log(`The sum of ${a} and ${b} is ${a + b}.`);
+// Output: The sum of 5 and 10 is 15.
+
+//Using Backticks for Multi-line Strings)
+
+const message = `Hello, Alamin!
+Welcome to ES6.`;
+console.log(message);
+
+
+class Vehicle {
+  constructor(brand, model, year) {
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+  }
+
+  displayInfo() {
+    return {year, brand, model}
+  };
+}
+
+// Creating an instance
+const car = new Vehicle("Toyota", "Corolla", 2022);
+console.log(car.displayInfo()); // Output: 2022 Toyota Corolla
+
+
+function studentReport(student1, student2, subject, grade) {
+  console.log(`📚 Subject: ${subject}`);
+  console.log(`✅ ${student1} and ${student2} both received a grade of ${grade}.`);
+}
+
+// Example Usage
+studentReport("Alamin", "Ahmed", "Mathematics", "A");
+// ✅ Output:
+// 📚 Subject: Mathematics
+// ✅ Alamin and Ahmed both received a grade of A.
